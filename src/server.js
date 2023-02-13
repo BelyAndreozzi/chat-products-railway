a5715fc290ec660f87e1072fb59dac6070eae73c
Bely <fbelen.andreozzi@gmail.com>
2023-02-13 20:35:18 -0300

    Port

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const args = parseArgs(process.argv.slice(2),optionsM)
 
 //sv
 const app = express()
-const PORT = args.p 
+const PORT = 8080
 const server = app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
